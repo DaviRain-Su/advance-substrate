3590d2889d4978e94640fda1ee58b46f1fb60dd1
Davirain <davirain.yin@gmail.com>
2022-09-10 10:18:42 +0800

    add random hash

--- substrate-node-template/pallets/kitties/src/lib.rs
+++ substrate-node-template/pallets/kitties/src/lib.rs
@@ -27,6 +27,7 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_support::traits::Randomness;
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::Hash;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -64,6 +65,10 @@ pub mod pallet {
 	#[pallet::getter(fn all_kitties_count)]
 	pub(super) type AllKittiesCount<T: Config> = StorageValue<_, u64, ValueQuery>;
 
+	#[pallet::storage]
+	#[pallet::getter(fn get_nonce)]
+	pub(super) type Nonce<T: Config> = StorageValue<_, u64, ValueQuery>;
+
 	// TODO Part II: Remaining storage item.
 
 	// TODO Part III: Our pallet's generic configuration.
@@ -90,6 +95,23 @@ pub mod pallet {
 			} else {
 				Gender::Female
 			}
+		}
+
+		fn increment_nonce() -> DispatchResult {
+			Nonce::<T>::try_mutate(|nonce| {
+				let next = nonce.checked_add(1).expect("Overflow"); // TODO: Add Error handle
+				*nonce = next;
+
+				Ok(().into())
+			})
+		}
+
+		// takes in an AccountId and returns the hash of a random seed, AccountId and the current nonce.
+		fn random_hash(sender: &T::AccountId) -> T::Hash {
+			let nonce = Self::get_nonce();
+			let seed = T::KittyRandomness::random_seed();
+
+			T::Hashing::hash_of(&(seed, &sender, nonce))
 		}
 		// TODO Part III: helper functions for dispatchable functions
 
